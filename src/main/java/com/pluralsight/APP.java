4532f2378f5ca1b74ff95c65fa5789ae301c514a
bbyhateshit <j.galeta@gmail.com>
2026-04-07 10:59:56 -0400

    Added a sout. Added 2 print statements.

--- src/main/java/com/pluralsight/APP.java
+++ src/main/java/com/pluralsight/APP.java
@@ -2,6 +2,8 @@ package com.pluralsight;
 
 public class APP {
     public static void main(String[] args) {
+        System.out.println("Oh my wow");
+        System.out.println("Let's get this bread");
 
     }
 }
